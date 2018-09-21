b90a620bdb3ccaaf35a343b56480ce940237f9c6
KVexcavator <str_token@mail.ru>
2018-09-22 02:52:44 +0600

    add data visit

--- app.rb
+++ app.rb
@@ -30,12 +30,34 @@ get '/visit' do
 end
 
 post '/visit' do
-	@master      = params[ :master]
+	
 	@name        = params[ :name]
 	@pfone       = params[ :pfone]
 	@data_time   = params[ :data_time]
+	@master      = params[ :master]
 	@color       = params[ :color]
+
+	#хеш сообщений об ошибке
+	hh={ :name       => "Enter name",
+       :pfone      => "Enter pfone",
+       :data_time  => "Enter date and time",
+	}
+
+	@error=hh.select{|key,_| params[key]==''}.values.join(', ')
+	if @error !=''
+		return erb :visit
+	end
+
+	if @name !="" and @pfone!="" and @data_time!=""
+			
+			Client.create :name => @name,
+		                :pfone => @pfone,
+			              :datestamp => @data_time,
+			              :master => @master,
+			              :color => @color
+
 	    
-	erb "<h2>Уважаемый #{@name}, Ваша запись сохранена, ждём Вас #{@data_time}.<h2/>"
-	
+	    erb "<h2>Уважаемый #{@name}, Ваша запись сохранена, ждём Вас #{@data_time}.<h2/>"
+	end
+		
 end
