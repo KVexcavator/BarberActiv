98a8cd4d191e7b5ec1366e1ffb3868cec4b0429f
KVexcavator <str_token@mail.ru>
2018-09-22 16:20:55 +0600

    add validates Client

--- app.rb
+++ app.rb
@@ -6,6 +6,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :pfone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Master < ActiveRecord::Base 
