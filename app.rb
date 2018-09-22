0e9e283efd8310d5b78cddd5668c95197200b71b
KVexcavator <str_token@mail.ru>
2018-09-22 17:58:54 +0600

    make links on main page, create master page

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :pfone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true
@@ -44,4 +44,9 @@ post '/visit' do
 		erb :visit
 	end
 		
+end
+
+get '/master/:num' do
+	num=params[:num]
+	erb "страница мастера "
 end
